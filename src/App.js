b68de547df16e34b24a3024599e3f1d7a3c531d8
Anubhav Kamath <akamath0606@gmail.com>
2024-03-23 16:39:59 -0400

    second set of changes to fix footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <br></br>
       <Square/>
       <Footer/>
     </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ import { BottomNavigation, Box } from "@mui/material";
 const Footer = () => {
     
     return(
-        <Box sx={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
-            <BottomNavigation sx={{backgroundColor: "#0B3853", height: "5vh", display: "flex", position: "fixed", bottom: 0, width: "100%", textAlign: "center", marginTop: "auto"}}>
+        <Box sx={{display: "flex", flexDirection: "column"}}>
+            <BottomNavigation sx={{backgroundColor: "#0B3853", height: "5vh", display: "flex", position: "fixed", bottom: 0, width: "100%", textAlign: "center"}}>
                 <div style={{marginRight: "auto", marginTop: "auto", marginBottom: "auto", paddingLeft:"16px", fontSize: "24px", color: "white" }}>
                     Thanks for playing Gridify!
                 </div>
