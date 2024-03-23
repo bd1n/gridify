a6eb97b771e0d9fda215f250489cddb5f1e046cb
Brennan Dolan <brennan0426@icloud.com>
2024-03-23 17:56:02 -0400

    did literally nothing, made header.css but it does nothing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
-import Square from './Components/Square';
 import Footer from './Components/Footer';
 import Board from './Components/Board';
 function App() {
