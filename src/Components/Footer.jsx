16cf255a77bffe1305f7a5dab3dba39f91cf7669
Anubhav Kamath <akamath0606@gmail.com>
2024-03-23 16:09:27 -0400

    fixed footer position

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { AppBar, BottomNavigation, Box } from "@mui/material";
+import { BottomNavigation, Box } from "@mui/material";
 
 const Footer = () => {
     
     return(
         <Box sx={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
-            <BottomNavigation sx={{backgroundColor: "#0B3853", height: "5vh", display: "flex", position: "static", textAlign: "center", marginTop: "auto"}}>
+            <BottomNavigation sx={{backgroundColor: "#0B3853", height: "5vh", display: "flex", position: "fixed", bottom: 0, width: "100%", textAlign: "center", marginTop: "auto"}}>
                 <div style={{marginRight: "auto", marginTop: "auto", marginBottom: "auto", paddingLeft:"16px", fontSize: "24px", color: "white" }}>
-                    Gridify
+                    Thanks for playing Gridify!
                 </div>
             </BottomNavigation>
         </Box>
