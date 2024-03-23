76b76c08e1f638ee27604b0b5b29ed2d72ae10b3
Brennan Dolan <brennan0426@icloud.com>
2024-03-23 17:02:50 -0400

    merge conflict 3

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,11 +5,7 @@ const Footer = () => {
     
     return(
         <Box sx={{display: "flex", flexDirection: "column"}}>
-<<<<<<< HEAD
             <BottomNavigation sx={{backgroundColor: "#0B3853", height: "5vh", display: "flex", position: "fixed", bottom: 0, textAlign: "center", width: "100%"}}>
-=======
-            <BottomNavigation sx={{backgroundColor: "#0B3853", height: "5vh", display: "flex", position: "fixed", bottom: 0, width: "100%", textAlign: "center"}}>
->>>>>>> b68de547df16e34b24a3024599e3f1d7a3c531d8
                 <div style={{marginRight: "auto", marginTop: "auto", marginBottom: "auto", paddingLeft:"16px", fontSize: "24px", color: "white" }}>
                     Thanks for playing Gridify!
                 </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { AppBar } from "@mui/material";
 
 const Header = () => {
-    //"#0B3853"
     return(
         <AppBar sx={{backgroundColor: "#0B3853", height: "8vh", display: "flex", textAlign: "center", position: "static", paddingBottom: "1vh"}}>
             <div style={{marginRight: "auto", marginTop: "auto", marginBottom: "auto", paddingLeft:"16px", fontSize: "24px" }}>
