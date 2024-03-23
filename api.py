ee0b462511fa639b5ba68c69feb950eab18b2c12
Devang Ray <devangray624@gmail.com>
2024-03-23 14:47:09 -0400

    uhhhhh

---
+++ api.py
@@ -0,0 +1,18 @@
+import requests
+import asyncio
+from aiohttp import ClientSession
+
+base_url = 'http://httpbin.org'
+
+def get_delay(seconds):
+    endpoint = f'/delay/{seconds}'
+    
+    print(f'Getting with {seconds} delay...')
+    
+    resp = requests.get(base_url+endpoint)
+    data = resp.json()
+    print(data)
+    
+get_delay(5)
+
+print('Okay! All finished getting.')
