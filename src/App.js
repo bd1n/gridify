0fdc7b2093db5cb2613bda07d370a890669949b1
Anubhav Kamath <akamath0606@gmail.com>
2024-03-23 17:12:21 -0400

    start of search box implementation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Header from './Components/Header';
 import Square from './Components/Square';
 import Footer from './Components/Footer';
+import SearchBar from './Components/SearchBar';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SearchBar/>
       <Square/>
       <Footer/>
     </div>
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+const SearchBar = () => 
+{
+    return
+    {
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    }
+}
+
+export default SearchBar;
