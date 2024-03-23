94e9dd1af4977f31f2901beeaddbe6d35756cc40
Anubhav Kamath <akamath0606@gmail.com>
2024-03-23 17:41:19 -0400

    added search bar to App.js

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TextField } from "@mui/material";
+import "../Styles/searchbar.css"
 
-const SearchBar = () => 
-{
-    return
-    {
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    }
+const SearchBar = () => {
+    return(
+        <div className="search-bar"> 
+            <TextField id="outlined-basic" label="Search for an artist" variant="outlined">
+            </TextField>
+        </div>
+    )
 }
 
 export default SearchBar;
