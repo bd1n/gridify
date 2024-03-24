49efda46f427e1b00e5fca4b7396fa24510f6ea9
Anubhav Kamath <akamath0606@gmail.com>
2024-03-24 02:31:25 -0400

    new logo dir

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { AppBar } from "@mui/material";
 
-
 const Header = () => {
     return(
         <AppBar sx={{backgroundColor: "#0B3853", height: "8vh", display: "flex", textAlign: "center", position: "static", paddingBottom: "1vh"}}>
             <div style={{marginRight: "auto", marginTop: "auto", marginBottom: "auto", paddingLeft:"16px", fontSize: "24px" }}>
-                <img src = "../images/Gridify_Logo.jpg" alt = ""></img>Gridify
+                <img src = "./images/Gridify_Logo.jpg" alt = ""/>Gridify
             </div>
         </AppBar>
     )
