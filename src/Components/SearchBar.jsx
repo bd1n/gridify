36e156e6bb5addb7c6f7589077592e076074b431
Anubhav Kamath <akamath0606@gmail.com>
2024-03-23 20:48:29 -0400

    implemented search submission

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { TextField } from "@mui/material";
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import "../Styles/searchbar.css"
 
+
 const SearchBar = () => {
+    const [searchText, setSearchText] = useState('');
+
     return(
         <div className="search-bar"> 
-            <TextField id="outlined-basic" label="Search for an artist" variant="outlined">
+            <TextField id="outlined-basic" label="Search for an artist" variant="outlined" onKeyDown={e=> e.key === 'Enter' ? setSearchText(e.target.value) : null}>
             </TextField>
+            <div>
+                Res = {searchText}
+            </div>
         </div>
     )
 }
+
 
 export default SearchBar;
